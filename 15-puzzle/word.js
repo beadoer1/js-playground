a733176a22ab1119df1f7b8eb30103f08b0b836e
Youn-Hwan <beadoer1@gmail.com>
2020-12-08 14:47:21 +0900

    game 객체의 메소드를 객체 내{}로 삽입
    - 좀 더 보기 좋다고 생각하여 정리
    - 객체 내 메소드에 전역변수 사용 시 전역변수가 사전에 선언되어야 사용 가능하다.

--- 15-puzzle/word.js
+++ 15-puzzle/word.js
@@ -3,26 +3,29 @@ var word2 = document.getElementById('word2');
 
 var words = "apple,linux,javascript,tutorial,codesquad,baby,girlfriend,legend".split(",");
 
-var game = {};
-game.choice = function() {
-    var idx = Math.floor(Math.random() * words.length);
-    return words[idx];
-}
+var game = {
+    choice : function(){
+        var idx = Math.floor(Math.random() * words.length);
+        return words[idx];
+    },
+    btns : [],
+    copyBtnText : function(){
+        for(var i = 0; i < this.word.length; i++){
+            this.btns[i].innerHTML = this.word[i];
+        }
+    },
+    updateDisplay : function(){
+        if(answer === game.word.join('')){
+            check.innerHTML = '일치합니다.';
+        } else{
+            check.innerHTML = '일치하지 않습니다.';
+        }
+    }
+};
+
 var answer = game.choice();
-game.word = answer.split('');
-game.btns = [];
-game.copyBtnText = function() {
-    for (var i = 0; i < this.word.length; i++){
-        this.btns[i].innerHTML = this.word[i];
-    }
-}
-game.updateDisplay = function() {
-    if(answer === game.word.join('')) {
-        check.innerHTML = '일치합니다.';
-    } else {
-        check.innerHTML = '일치하지 않습니다.';
-    }
-}
+game.word = answer.split(''); //answer가 정의되어있지 않아 answer 이후 선언 필요하다.(game 객체 내에 넣으면 오류 뜬다.)
+word1.innerHTML = answer;
 
 for (var i = 0; i < answer.length; i++) {
     var btn = document.createElement('button');
@@ -60,13 +63,11 @@ var lshift = function(event) {
 //shuffle
 
 var toggle = Math.floor(Math.random() * 2) === 0;
-
 if(toggle) {
     swap();
-}
+};
 
 var n = Math.floor(Math.random() * answer.length);
-
 for(var i = 0; i < n; i++) {
     rshift();
-}
+};
